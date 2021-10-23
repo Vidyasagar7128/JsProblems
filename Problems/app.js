d2411c8bb51e00212c3c3d735a4e9f900086cd84
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-23 08:47:26 +0530

    Added: Sequence Practice

--- Problems/app.js
+++ Problems/app.js
@@ -1,1 +1,20 @@
-console.log("WelCome to Javascript")
+// Random Numbers
+var ran1 = Math.floor(Math.random() * 5) + 1;
+var ran2 = Math.floor(Math.random() * 5) + 1;
+console.log(ran1, " ", ran2)
+console.log(ran1 + ran2)
+// Unit Conversion
+var oneFt = 12
+var unit = 42 / oneFt
+console.log("In Ft: ", unit)
+//Plot of Area
+var val1 = 60 * 0.3048
+val2 = 40 * 0.3048
+console.log("60 in Meter :", val1, "40 in Meter :", val2)
+var totalArea = val1 * val2
+var area = totalArea * 0.00024711
+var plot = area * 25
+console.log("25 Flots: ", plot)
+
+
+
