ae00aa71a8241027630ef9b0c8053880062801e1
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-26 21:14:21 +0530

    Some Changes

--- functions/palindrome.js
+++ functions/palindrome.js
@@ -5,12 +5,15 @@ var num = readlineSync.questionInt('please enter the number : ');
 var d;
 var rev = 0;
 var temp = num;
-while (num > 0) {
-    d = num % 10;
-    rev = rev * 10 + d;
-    num = parseInt(num / 10);
+function palindromeNumber(num) {
+    while (num > 0) {
+        d = num % 10;
+        rev = rev * 10 + d;
+        num = parseInt(num / 10);
+    }
+    if (temp == rev)
+        console.log("palindrome");
+    else
+        console.log("not palindrome");
 }
-if (temp == rev)
-    console.log("palindrome");
-else
-    console.log("not palindrome");
+palindromeNumber(num);
